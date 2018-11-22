df5d592a7003754bfab13979ef3841d9d49b5170
okumura <risa.okumura@rakus.co.jp>
2018-11-22 17:38:49 +0900

    注文トッピングのリポジトリの注文トッピングの登録機能を修正

--- src/main/java/jp/co/rakus/ec2018c/repository/OrderToppingRepository.java
+++ src/main/java/jp/co/rakus/ec2018c/repository/OrderToppingRepository.java
@@ -8,9 +8,11 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
 
 import jp.co.rakus.ec2018c.domain.OrderTopping;
 
+@Repository
 public class OrderToppingRepository {
 	
 	@Autowired
