f119e64075c21acbbc080b889355b156784a2c02
uemurakento <uemurakento963@gmail.com>
2018-11-22 17:14:40 +0900

    OrderRepositoryのfindByUserIdAndStatusメソッドの作成途中(SQL文は作成済み)

--- src/main/java/jp/co/rakus/ec2018c/repository/OrderRepository.java
+++ src/main/java/jp/co/rakus/ec2018c/repository/OrderRepository.java
@@ -1,12 +1,32 @@
 package jp.co.rakus.ec2018c.repository;
 
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
 import jp.co.rakus.ec2018c.domain.Order;
 
 @Repository
 public class OrderRepository {
+	private static final ResultSetExtractor<Order> ORDER_RESULT_SET_EXTRACTOR = (rs) ->{
+		Order order = new Order();
+		while(rs.next()) {
+			
+		}
+		return order;
+	};
+	
 	public Order findByUserIdAndStatus(Integer userId,Integer status){
+		String sql = "SELECT o.id order_id,o.user_id user_id,o.status order_status,o.total_price order_total_price,"
+				+ "o.order_date order_date,o.destination_name order_destination_name,o.destination_email order_destination_email,"
+				+ "o.destination_zipcode order_destination_zipcode,o.destination_address order_destination_address,"
+				+ "o.destination_tel order_destination_tel,o.delivery_time order_delivery_time,o.payment_method order_payment_method,"
+				+ "oi.id order_item_id,oi.item_id item_id,oi.quantity order_item_quantity,oi.size order_item_size,i.name item_name,"
+				+ "i.description item_description,i.price_m item_price_m,i.price_l item_price_l,i.image_path item_image_path,"
+				+ "i.deleted item_deleted,ot.id order_topping_id,ot.topping_id topping_id,t.name topping_name,t.price_m topping_price_m,"
+				+ "t.price_l topping_price_l FROM orders o JOIN order_items oi ON o.id = oi.order_id "
+				+ "JOIN order_toppings ot ON oi.id = ot.order_item_id INNER JOIN items i ON oi.item_id = i.id "
+				+ "INNER JOIN toppings t ON ot.topping_id = t.id ORDER BY i.name ,t.name; ";
+		
 		return null;
 	}
 }
