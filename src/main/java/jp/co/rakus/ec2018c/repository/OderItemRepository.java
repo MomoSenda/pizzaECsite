489cddfd37b586432f69d2dd00e5f1ad19098137
okumura <risa.okumura@rakus.co.jp>
2018-11-22 14:30:50 +0900

    注文商品のリポジトリを作成し、注文商品の登録機能を実装

---
+++ src/main/java/jp/co/rakus/ec2018c/repository/OderItemRepository.java
@@ -0,0 +1,57 @@
+package jp.co.rakus.ec2018c.repository;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import jp.co.rakus.ec2018c.domain.OrderItem;
+
+/**
+ * 注文商品のリポジトリ.
+ * 
+ * @author risa.okumura
+ *
+ */
+@Repository
+public class OderItemRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	private SimpleJdbcInsert insert;
+	
+	/**
+	 * 注文商品が登録されたときに採番されたIDを取得する.
+	 */
+	@PostConstruct
+	public void init() {
+		SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert((JdbcTemplate)namedParameterJdbcTemplate.getJdbcOperations());
+		SimpleJdbcInsert withTableName = simpleJdbcInsert.withTableName("order_items");
+		insert = withTableName.usingGeneratedKeyColumns("id");
+	}
+	
+	/**
+	 * ショッピングカートに商品を追加する.
+	 * 
+	 * @param orderItem 注文商品の情報
+	 * @return　IDを採番された注文商品の情報
+	 */
+	public OrderItem save (OrderItem orderItem) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(orderItem);
+		Number key = insert.executeAndReturnKey(param);
+		orderItem.setId(key.intValue());
+
+		return orderItem;
+	}
+	
+
+}
