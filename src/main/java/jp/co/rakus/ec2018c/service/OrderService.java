a7ed311c1470891e4997014c5b19b32324cc199b
uemurakento <uemurakento963@gmail.com>
2018-11-22 13:50:53 +0900

    OrderServiceの完成

--- src/main/java/jp/co/rakus/ec2018c/service/OrderService.java
+++ src/main/java/jp/co/rakus/ec2018c/service/OrderService.java
@@ -1,12 +1,30 @@
 package jp.co.rakus.ec2018c.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jp.co.rakus.ec2018c.domain.Order;
+import jp.co.rakus.ec2018c.repository.OrderRepository;
 
+/**
+ * 注文情報を表示するためのサービス.
+ * 
+ * @author kento.uemura
+ *
+ */
 @Service
 public class OrderService {
+	@Autowired
+	OrderRepository orderRepository;
+	
+	/**
+	 * UserId,Statusを指定して注文情報を取得する.
+	 * 
+	 * @param userId ログインユーザのID
+	 * @param status 注文の状態 (0:未注文,1:未入金,2:入金済み,3:発送済み,9:キャンセル)
+	 * @return 取得した注文情報
+	 */
 	public Order findByUserIdAndStatus(Integer userId,Integer status) {
-		return null;
+		return orderRepository.findByUserIdAndStatus(userId, status);
 	}
 }
