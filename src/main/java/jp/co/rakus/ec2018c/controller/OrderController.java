645b83b207db917d5b9c3fa10d88254e53b048be
uemurakento <uemurakento963@gmail.com>
2018-11-22 17:13:47 +0900

    OrderControllerの作成

--- src/main/java/jp/co/rakus/ec2018c/controller/OrderController.java
+++ src/main/java/jp/co/rakus/ec2018c/controller/OrderController.java
@@ -1,15 +1,37 @@
 package jp.co.rakus.ec2018c.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.rakus.ec2018c.domain.Order;
+import jp.co.rakus.ec2018c.service.OrderService;
+
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+	@Autowired
+	private OrderService orderService;
+	
+	public Integer UNORDERED_ID = 0;
+	
+	/**
+	 * 注文情報を表示する.
+	 * 注文情報をuseridとstatus(0:未注文)で調べてmodelに格納する
+	 * 
+	 * @param model　モデル
+	 * @return 注文表示画面
+	 */
 	@RequestMapping("/orderconfirm")
 	public String index(Model model){
-		//注文情報をOrderに詰めてモデルにセットする
+		Integer status = UNORDERED_ID; 
+		
+		//TODO:ログインフィルターを実装したらここでuserIdにログインユーザのidを入れる(仮で0を入れている)
+		Integer userId = 1;
+		
+		Order order = orderService.findByUserIdAndStatus(userId, status);
+		model.addAttribute("order", order);
 		return "orderconfirm";
 	}
 }
