cd1a615b1f4efade81c94ee38e25b17c1373b688
okumura <risa.okumura@rakus.co.jp>
2018-11-22 10:55:34 +0900

    注文商品のドメインを作成

---
+++ src/main/java/jp/co/rakus/ec2018c/domain/OrdemItem.java
@@ -0,0 +1,87 @@
+package jp.co.rakus.ec2018c.domain;
+
+import java.util.List;
+
+/**
+ * 注文商品の情報を表すドメイン.
+ * 
+ * @author risa.okumura
+ *
+ */
+public class OrdemItem {
+	
+	
+	/** 注文商品のID	 */
+	private Integer id;
+	/** 商品のID	 */
+	private Integer itemId;
+	/** 注文のID	 */
+	private Integer orderId;
+	/** 数量	 */
+	private Integer quantity;
+	/** サイズ　*/
+	private Character size;
+	/** サイズ　*/
+	private Item item;
+	/** 商品　*/
+	private List<Toppoing> orderToppingList;
+	
+	/**
+	 * 合計金額を計算する.
+	 * 
+	 * @return 合計金額
+	 */
+	public int getSubTotal() {
+		return 0;
+	}
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getItemId() {
+		return itemId;
+	}
+	public void setItemId(Integer itemId) {
+		this.itemId = itemId;
+	}
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Character getSize() {
+		return size;
+	}
+	public void setSize(Character size) {
+		this.size = size;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public List<Toppoing> getOrderToppingList() {
+		return orderToppingList;
+	}
+	public void setOrderToppingList(List<Toppoing> orderToppingList) {
+		this.orderToppingList = orderToppingList;
+	}
+	
+	
+	
+	
+
+}
