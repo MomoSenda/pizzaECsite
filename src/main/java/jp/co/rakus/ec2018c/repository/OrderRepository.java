c43559bfe3272230d2c40fd3ccbc8b568a328d17
uemurakento <uemurakento963@gmail.com>
2018-11-22 17:47:00 +0900

    OrderRepositoryのsaveメソッドの作成

--- src/main/java/jp/co/rakus/ec2018c/repository/OrderRepository.java
+++ src/main/java/jp/co/rakus/ec2018c/repository/OrderRepository.java
@@ -1,12 +1,23 @@
 package jp.co.rakus.ec2018c.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import jp.co.rakus.ec2018c.domain.Order;
 
 @Repository
 public class OrderRepository {
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	public static final String TABLE_NAME = "orders";
+	
+	//作成途中
 	private static final ResultSetExtractor<Order> ORDER_RESULT_SET_EXTRACTOR = (rs) ->{
 		Order order = new Order();
 		while(rs.next()) {
@@ -15,6 +26,7 @@ public class OrderRepository {
 		return order;
 	};
 	
+	//作成途中
 	public Order findByUserIdAndStatus(Integer userId,Integer status){
 		String sql = "SELECT o.id order_id,o.user_id user_id,o.status order_status,o.total_price order_total_price,"
 				+ "o.order_date order_date,o.destination_name order_destination_name,o.destination_email order_destination_email,"
@@ -28,5 +40,34 @@ public class OrderRepository {
 				+ "INNER JOIN toppings t ON ot.topping_id = t.id ORDER BY i.name ,t.name; ";
 		
 		return null;
+	}
+	
+	private SimpleJdbcInsert insert;
+	
+	public void init() {
+		SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert((JdbcTemplate)template.getJdbcOperations());
+		SimpleJdbcInsert withTableName = simpleJdbcInsert.withTableName(TABLE_NAME);
+		insert = withTableName.usingGeneratedKeyColumns("id");
+	}
+	
+	/**
+	 * saveメソッド.
+	 * 
+	 * @param order insert,updateするOrderドメイン.
+	 * @return insertの場合は自動採番されたidが入ったOrderドメイン、updateの場合は引数をそのまま返す
+	 */
+	public Order save(Order order) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(order);
+		if(order.getId() == null) {
+			Number key = insert.executeAndReturnKey(param);
+			order.setId(key.intValue());
+		}else {
+			String sql = "UPDATE orders SET user_id=:userId status=:status,total_price=:totalPrice,order_date=:orderDate,"
+					+ "destination_name=:destinationName,destination_email=:destinationEmail,destination_zipcode=:destinationZipcode,"
+					+ "destination_address=:destinationAddress,destination_tel=:destinationTel,delivery_time=:deliveryTime,"
+					+ "payment_method=:paymentMethod WHERE id=:id;";
+			template.update(sql, param);
+		}
+		return order;
 	}
 }
