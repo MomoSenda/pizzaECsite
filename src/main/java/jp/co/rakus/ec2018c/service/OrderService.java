e40eaaf4769ea9363bdda69cfafec7c7af2c21b9
uemurakento <uemurakento963@gmail.com>
2018-11-22 13:43:42 +0900

    OrderServiceの仮作成

---
+++ src/main/java/jp/co/rakus/ec2018c/service/OrderService.java
@@ -0,0 +1,12 @@
+package jp.co.rakus.ec2018c.service;
+
+import org.springframework.stereotype.Service;
+
+import jp.co.rakus.ec2018c.domain.Order;
+
+@Service
+public class OrderService {
+	public Order findByUserIdAndStatus(Integer userId,Integer status) {
+		return null;
+	}
+}
