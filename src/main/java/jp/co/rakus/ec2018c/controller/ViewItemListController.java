d7ee260f5d5702799c1bfde77ceb9356d57613c1
momosenda <momo.senda@rakus.co.jp>
2018-11-23 17:13:19 +0900

    全件表示ができなくなってたので、微調整

--- src/main/java/jp/co/rakus/ec2018c/controller/ViewItemListController.java
+++ src/main/java/jp/co/rakus/ec2018c/controller/ViewItemListController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.rakus.ec2018c.domain.Item;
+import jp.co.rakus.ec2018c.form.RegisterUserForm;
+import jp.co.rakus.ec2018c.form.SearchItemForm;
 import jp.co.rakus.ec2018c.service.ViewItemListService;
 
 /**
@@ -25,6 +28,11 @@ public class ViewItemListController {
 	@Autowired
 	private  ViewItemListService viewItemListService;
 	
+	@ModelAttribute
+	public SearchItemForm setUpForm() {
+		return new SearchItemForm();
+	}
+
 	/**
 	 * itemの全情報を取得し、画面に表示する.
 	 * @param model モデル
