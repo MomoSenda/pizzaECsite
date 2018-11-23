0a27646426ef72e5dd195566567c065841b110e4
momosenda <momo.senda@rakus.co.jp>
2018-11-23 16:47:45 +0900

    findByName(String name)の追加

--- src/main/java/jp/co/rakus/ec2018c/service/ViewItemListService.java
+++ src/main/java/jp/co/rakus/ec2018c/service/ViewItemListService.java
@@ -19,8 +19,24 @@ public class ViewItemListService {
 	@Autowired
 	private ItemRepository itemRepository;
 
+	/**
+	 * 商品の全ての情報を取得するサービスクラス
+	 * 
+	 * @return　取得した商品の情報
+	 */
 	public List<Item> findAll() {
 		return itemRepository.findAll();
+	}
+	
+	/**
+	 * 名前から商品を検索する
+	 * 
+	 * @param name　商品の名前
+	 * 
+	 * @return　検索された商品情報
+	 */
+	public List<Item> findByName(String name){
+		return itemRepository.findByName(name);
 	}
 
 }
