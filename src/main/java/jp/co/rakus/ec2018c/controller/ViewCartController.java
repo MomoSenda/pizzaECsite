6c2be2db86e4558c49fc8d8c40f564bc6eba3ea7
okumura <risa.okumura@rakus.co.jp>
2018-11-27 18:02:20 +0900

    未ログインの状態でカートを表示できるようセッションIDを取得する機能を実装

--- src/main/java/jp/co/rakus/ec2018c/controller/ViewCartController.java
+++ src/main/java/jp/co/rakus/ec2018c/controller/ViewCartController.java
@@ -1,5 +1,7 @@
 package jp.co.rakus.ec2018c.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -26,6 +28,9 @@ public class ViewCartController {
 	@Autowired
 	private ViewCartService service;
 	
+	@Autowired
+	private HttpSession session;
+	
 	/**
 	 * ユーザーIDをもとに、ショッピングカートの中身（未購入状態のも）の一覧を表示する.
 	 * 
@@ -37,9 +42,16 @@ public class ViewCartController {
 	@RequestMapping("/viewCart")
 	public String viewCart(@AuthenticationPrincipal LoginUser loginUser ,Integer status,Model model) {
 		
+		
 		//ログイン認証からユーザー情報を取得し、ユーザーIDに代入.
-		User user = loginUser.getUser();
-		Integer userId = user.getId();
+		Integer userId;
+		
+		if(loginUser == null) {
+			userId = Integer.parseInt(session.getId().replaceAll("[A-Z]+", "").substring(0, 8));
+		}else {
+			User user = loginUser.getUser();
+			userId = user.getId();
+		}
 		
 		//未購入の注文情報を指定.
 		status = 0;
