8f590c910cc6748fc28e953434b12e9ef55e3464
momosenda <momo.senda@rakus.co.jp>
2018-11-27 14:05:17 +0900

    calc.jsの新規作成

---
+++ src/main/webapp/js/calc.js
@@ -0,0 +1,25 @@
+$(function(){
+	
+	$('.size').change(function(){
+		calc();
+
+	});
+	
+	$('.toppingList').change(function() {
+		calc();
+	});
+	
+    $('.form-control').change(function() {
+    	calc();
+    });
+ 
+    function calc() {
+    	var str = $('.size:checked').val();
+    	var cnt = $('.toppingList:checkbox:checked').length;
+    	var quantity = $('.form-control option:selected').val();
+    	console.log("サイズ："+ str);
+    	console.log("トッピング："+ cnt);
+    	console.log("枚数："+ quantity);
+    	  return "null";
+    	};
+});
