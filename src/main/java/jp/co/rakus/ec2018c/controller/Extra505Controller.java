fc1c5098e0d522f90db941fa7bc65a37667a9934
momosenda <momo.senda@rakus.co.jp>
2018-11-29 16:58:02 +0900

    Extra500controllerの新規作成

---
+++ src/main/java/jp/co/rakus/ec2018c/controller/Extra505Controller.java
@@ -0,0 +1,30 @@
+package jp.co.rakus.ec2018c.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.rakus.ec2018c.domain.Item;
+import jp.co.rakus.ec2018c.service.RecommendService;
+
+@Controller
+@RequestMapping("/")
+@Transactional
+public class Extra505Controller {
+
+	@Autowired
+	private RecommendService recommendService;
+	
+	@RequestMapping("/500")
+	
+	public String index(Model model) {
+		List<Item> itemRecommendList = recommendService.recommend();
+		model.addAttribute("itemRecommendList",itemRecommendList);
+		
+		return "500";
+	}
+}
