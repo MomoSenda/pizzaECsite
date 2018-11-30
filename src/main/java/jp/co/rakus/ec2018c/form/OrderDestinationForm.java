63f5de7a562a71d0fba821ba5269729ad4229e00
yuta.ikeda <yuta.ikeda@rakus.co.jp>
2018-11-30 09:19:47 +0900

    お届け先情報　orderdestinationformの微調整.

--- src/main/java/jp/co/rakus/ec2018c/form/OrderDestinationForm.java
+++ src/main/java/jp/co/rakus/ec2018c/form/OrderDestinationForm.java
@@ -1,6 +1,5 @@
 package jp.co.rakus.ec2018c.form;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
